e1305b36ee204f0de12050927e8884f3a4a05c26
HRK <hkloeckner@dhcp1790.mpifr-bonn.mpg.de>
2026-02-10 11:13:07 +0100

    setting testing

--- daskms_ASTROKIT/GET_MS_INFO.py
+++ daskms_ASTROKIT/GET_MS_INFO.py
@@ -16,8 +16,7 @@ import json
 import numpy as np
 from astropy.time import Time
 
-import daskmsASTROKIT.DASK_MS_WERKZEUGKASTEN as INFMS
-
+import  daskms_ASTROKIT.daskmsASTROKIT.DASK_MS_WERKZEUGKASTEN as INFMS
 
 def main():
 
